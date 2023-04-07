96b378d31b53e826818b1fbf67bb07f7c342a61c
hoprik <87880130+hoprik@users.noreply.github.com>
2023-04-07 17:29:47 +0300

    Delete php directory

--- php/server.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host_db = "127.0.0.1";
-$name_db = "root";
-$password_db = "";
-$db_name = "messager"
-?>
--- php/test.js
+++
@@ -1,3 +0,0 @@
-export function repo(req, res){
-    res.send(200, { response: 'response 1' });
-}
